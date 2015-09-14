35a2a6ff72de40e191ad67bc2d87eda3c387930d
Kyle Pratuch & Annie Hoogendoorn <student@epicodus.com>
2015-09-14 16:50:56 -0700

    add coments

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $("#add-address").click(function() {
+        //Add new input fields for additional addresses
         $("#new-addresses").append(  '<div class="new-address">' +
                                         '<div class="form-group">' +
                                             '<label for="new-street">Street</label>' +
@@ -19,10 +20,11 @@ $(document).ready(function() {
 
     $("form#new-contact").submit(function(event) {
         event.preventDefault();
-
+        //Get values from form and use as variables
         var inputtedFirstName = $("input#new-first-name").val();
         var inputtedLastName = $("input#new-last-name").val();
 
+        //Create new contact object (similar to php constructor)
         var newContact = { firstName: inputtedFirstName, lastName: inputtedLastName, addresses: [] };
 
         $(".new-address").each(function() {
@@ -31,11 +33,14 @@ $(document).ready(function() {
             var inputtedState = $(this).find("input.new-state").val();
 
             var newAddress = { street: inputtedStreet, city: inputtedCity, state: inputtedState };
+            //Pushes address objects to addresses array in newContact object
             newContact.addresses.push(newAddress);
         });
 
+        //Add to list of contacts below form
         $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
 
+        //Click on contact in list to view properties on right
         $(".contact").last().click(function() {
             $("#show-contact").show();
 
@@ -49,6 +54,7 @@ $(document).ready(function() {
             });
         });
 
+        //Clear all form values
         $("input#new-first-name").val("");
         $("input#new-last-name").val("");
         $("input.new-street").val("");
