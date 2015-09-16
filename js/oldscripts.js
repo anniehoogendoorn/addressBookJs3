8f8da3f760eff3e498471fa08e27b0b73f47038e
Annie <anniehoogendoorn@gmail.com>
2015-09-15 20:05:02 -0700

    Add Address contructor and prototype to scripts.js and specs.js.

---
+++ js/oldscripts.js
@@ -0,0 +1,86 @@
+//Creates new contact object with a function constructor (similar to php constructor)
+function Contact(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.addresses = [];
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
+
+Address.prototype.fullAddress = function() {
+  return this.street + ", " + this.city + ", " this.state;
+}
+
+$(document).ready(function() {
+  $("#add-address").click(function() {
+      //Add new input fields for additional addresses
+      $("#new-addresses").append(  '<div class="new-address">' +
+                                      '<div class="form-group">' +
+                                          '<label for="new-street">Street</label>' +
+                                          '<input type="text" class="form-control new-street">' +
+                                      '</div>' +
+                                      '<div class="form-group">' +
+                                          '<label for="new-city">City</label>' +
+                                          '<input type="text" class="form-control new-city">' +
+                                      '</div>' +
+                                      '<div class="form-group">' +
+                                          '<label for="new-state">State</label>' +
+                                          '<input type="text" class="form-control new-state">' +
+                                      '</div>' +
+                                  '</div>');
+
+  });
+
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+    //Get values from form and use as variables
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+
+
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+    $(".new-address").each(function() {
+      var inputtedStreet = $(this).find("input.new-street").val();
+      var inputtedCity = $(this).find("input.new-city").val();
+      var inputtedState = $(this).find("input.new-state").val();
+
+      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState);
+      //Pushes address objects to addresses array in newContact object
+      newContact.addresses.push(newAddress);
+    });
+
+    //Add to list of contacts below form
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+    //Click on contact in list to view properties on right
+    $(".contact").last().click(function() {
+        $("#show-contact").show();
+
+        $("#show-contact h2").text(newContact.fullName());
+        $(".first-name").text(newContact.firstName);
+        $(".last-name").text(newContact.lastName);
+
+        $("ul#addresses").text("");
+        newContact.addresses.forEach(function(address) {
+            $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
+        });
+    });
+
+    //Clear all form values
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input.new-street").val("");
+    $("input.new-city").val("");
+    $("input.new-state").val("");
+
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -10,4 +10,21 @@ describe('Contact', function() {
     var testContact = new Contact("Sherlock", "Holmes");
     expect(testContact.fullName()).to.equal("Sherlock Holmes");
   });
+  // debugger;
+
+});
+
+describe('Address', function() {
+  it("creates a new address with the given specifications", function() {
+    var testAddress = new Address("123 Main St", "Test City","Test State");
+    expect(testAddress.street).to.equal("123 Main St");
+    expect(testAddress.city).to.equal("Test City");
+    expect(testAddress.state).to.equal("Test State");
+  });
+
+  it("adds the fullAddress method to all addresses", function() {
+    var testAddress = new Address("123 Main St", "Test City","Test State");
+    expect(testAddress.fullAddress()).to.equal("123 Main St, Test City, Test State");
+  });
+debugger;
 });
